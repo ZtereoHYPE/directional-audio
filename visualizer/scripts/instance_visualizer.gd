23e482c726725daa06ddd1a59ad6e11ead496ed8
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2025-07-18 11:37:01 +0200

    visualizer: add instance visualizer

---
+++ visualizer/scripts/instance_visualizer.gd
@@ -0,0 +1,31 @@
+extends MultiMeshInstance3D
+
+@onready
+var instance_mesh := $Instance.mesh as SphereMesh;
+
+@onready
+var listener := $"../../Player/Head/AudioListenerNode"
+
+func _ready() -> void:
+	cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF;
+	
+	multimesh = MultiMesh.new();
+	multimesh.transform_format = MultiMesh.TRANSFORM_3D;
+	multimesh.mesh = instance_mesh;
+	multimesh.instance_count = 1024 * 64;
+
+
+func _process(delta: float) -> void:
+	global_position = listener.global_position
+
+
+func _on_audio_listener_node_visualization_data_received(data: GodotVisualizationData) -> void:
+	build_instances(data.instances);
+
+
+func build_instances(instances: PackedVector3Array):
+	multimesh.visible_instance_count = instances.size();
+	
+	for idx in range(instances.size()):
+		var offset := instances[idx];
+		multimesh.set_instance_transform(idx, Transform3D().translated(offset))
--- visualizer/scripts/ray_visualizer.gd
+++ visualizer/scripts/ray_visualizer.gd
@@ -2,6 +2,12 @@ extends MeshInstance3D
 
 const BOUNCE_AMOUNT: int = 4;
 
+@export
+var color_map: Dictionary[float, Color] = {
+	1.0: Color(0.9, 0, 0),
+	2.0: Color(0, 0.3, 0.9),
+}
+
 func _ready() -> void:
 	cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF;
 	global_position = Vector3.ZERO;
@@ -13,13 +19,14 @@ func _on_audio_listener_node_visualization_data_received(data: GodotVisualizatio
 
 func build_ray_mesh(origin: Vector3, rays: PackedVector4Array) -> ImmediateMesh:
 	var new_mesh := ImmediateMesh.new();
-	
 	var material := StandardMaterial3D.new();
-	material.albedo_color = Color.RED;
+	material.vertex_color_use_as_albedo = true;
+	material.shading_mode = BaseMaterial3D.SHADING_MODE_PER_VERTEX;
+	new_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material);
 	
 	var index := 0;
-	var total_segments := 0;
 	while (index < rays.size() / BOUNCE_AMOUNT):
+		var vertices: Array[Vector4] = [];
 		var ray_index: int = index * 4;
 		
 		# If the ray gets nowhere, skip this one
@@ -27,20 +34,26 @@ func build_ray_mesh(origin: Vector3, rays: PackedVector4Array) -> ImmediateMesh:
 			index += 1;
 			continue;
 		
-		# Start a new line
-		new_mesh.surface_begin(Mesh.PRIMITIVE_LINE_STRIP, material);
-		new_mesh.surface_add_vertex(origin);
-		
 		# For every bounce, if it exists then extend the line
-		for bounce_index: int in range(BOUNCE_AMOUNT):
+		var source_reached := false;
+		for bounce_index in range(BOUNCE_AMOUNT):
 			var pos := rays[ray_index + bounce_index];
 			if pos.w < 0.5:
 				break; # if the bounce doesn't exist stop the iteration
+			elif pos.w == 2.0:
+				source_reached = true;
 			
-			total_segments += 1;
-			new_mesh.surface_add_vertex(Vector3(pos.x, pos.y, pos.z));
-			
-		index += 4;
-		new_mesh.surface_end();
+			vertices.push_back(pos);
+		
+		if source_reached:
+			var previous := Vector3(origin.x, origin.y, origin.z);
+			for vtx in vertices:
+				new_mesh.surface_set_color(color_map[vtx.w])
+				new_mesh.surface_add_vertex(previous);
+				previous = Vector3(vtx.x, vtx.y, vtx.z);
+				new_mesh.surface_add_vertex(previous);
+		 
+		index += 1;
 	
+	new_mesh.surface_end();
 	return new_mesh;
